bba3ffd6eeb33e9d10d68a6067c09a7ad8d3067d
Jordan Catibog <jcatibog@cdsglobal.ca>
2016-02-05 15:54:16 -0500

    updated slim3 to 3.1.0

--- slim3/public/index.php
+++ slim3/public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
@@ -14,17 +13,17 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../app/settings.php';
+$settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../app/dependencies.php';
+require __DIR__ . '/../src/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../app/middleware.php';
+require __DIR__ . '/../src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../app/routes.php';
+require __DIR__ . '/../src/routes.php';
 
-// Run!
+// Run app
 $app->run();
