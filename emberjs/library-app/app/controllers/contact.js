fb2f715078acdcbfab9fe34ff3dc084e2e200d09
Timothy Catibog <timothycatibog@gmail.com>
2016-02-11 17:18:07 -0500

    added admin section

--- emberjs/library-app/app/controllers/contact.js
+++ emberjs/library-app/app/controllers/contact.js
@@ -1,20 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  emailAddress: '',
+  email: '',
   message: '',
 
   // computed properties
-  isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isValidEmail: Ember.computed.match('email', /^.+@.+\..+$/),
   isValidMessage: Ember.computed.gte('message.length', 5),
   isValidForm: Ember.computed.and('isValidEmail', 'isValidMessage'),
   isButtonDisabled: Ember.computed.not('isValidForm'),
 
   actions: {
     saveContactMessage() {
-      alert(`${this.get('emailAddress')}: ${this.get('message')}`);
+      alert(`${this.get('email')}: ${this.get('message')}`);
       this.set('commentReceived', true);
-      this.set('emailAddress', '');
+      this.set('email', '');
       this.set('message', '');
     }
   }
--- emberjs/library-app/config/environment.js
+++ emberjs/library-app/config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    firebase: 'https://emberjs-library-app.firebaseio.com/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
